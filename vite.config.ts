ff2ed11a071c80e93885cac581daebb4768f4e73
65061447 <smith.dip@spumail.net>
2026-02-23 14:11:33 +0700

    a little bit of change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/A_Feather_In_Memory/", 
+  base: "/Mumei/", 
   
   server: {
     host: "::",
